5d4aed726ca509ba420992757b5d08c064bbd3f8
Marc JEAN PIERRE <coursuniv.jeanpierre@gmail.com>
2021-02-25 20:01:27 +0100

    👌 IMPROVE: login page

--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MatInputModule} from '@angular/material/input';
+import { MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
 import { AppRoutingModule } from './app.routing.module';
+import {MatCardModule} from '@angular/material/card';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './authantification/login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
